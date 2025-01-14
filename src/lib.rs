38904783ad8f4b79da9474e60e7b27697ce1b390
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2025-01-13 22:18:40 -0800

    beginning tests but major cope

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,62 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+// Declare submodules
+pub mod auth;
+pub mod core;
+pub mod models;
+pub mod utils;
+
+// Declare modules in this current directory
+pub mod api;
+pub mod error;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use dotenv::dotenv;
+    use std::env;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    use crate::auth::dev::{APIAccount, LoginResponse};
+    use crate::auth::keys::Keys;
+    use crate::auth::credentials::Credentials;
+
+    use crate::api::api;
+
+    use anyhow;
+
+    use reqwest;
+    use serde_json::{from_str, Value};
+
+    fn get_credentials() -> Credentials {
+        dotenv().ok();
+
+        let email = env::var("EMAIL").expect("EMAIL environment variable missing");
+        let password = env::var("PASSWORD").expect("PASSWORD environment variable missing");
+
+        Credentials::builder()
+            .add_credential(email, password)
+            .build()
+    }
+
+    #[tokio::test]
+    async fn test_login() -> anyhow::Result<()> {
+        let credentials: Credentials = get_credentials();
+
+        println!("{:#?}", credentials);
+
+        let email = credentials
+            .0
+            .first()
+            .ok_or_else(|| anyhow::anyhow!("No credentials found"))?
+            .email();
+
+        let password = credentials
+            .0
+            .first()
+            .ok_or_else(|| anyhow::anyhow!("No credentials found"))?
+            .password();
+
+        let login_response = APIAccount::login(email, password).await?;
+
+        println!("{:#?}", login_response);
+
+        Ok(())
     }
 }
